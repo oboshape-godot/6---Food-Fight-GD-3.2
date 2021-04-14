cc0fb6365321334e9da2f13dfc103593d2f1cbfd
OboShape <dar_mcbain@yahoo.co.uk>
2021-04-14 09:26:43 +0100

    99 - Mouse Look

--- Scenes/Characters/Players/Player.gd
+++ Scenes/Characters/Players/Player.gd
@@ -6,14 +6,30 @@ var motion = Vector3()
 
 var movement_state = 0 # idle is 0, run is 1
 
+export var mouse_sensitivity = 1200
+
 const MIN_BLEND_SPEED = 0.125 # min movement threshold before starting blend anims
 const BLEND_TO_RUN = 0.075
 const BLEND_TO_IDLE = 0.075
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _physics_process(delta):
 	move()
 	animate()
 
+
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+	
+	if event is InputEventMouseMotion:
+		rotation = h_camera_rotation(-event.relative.x/mouse_sensitivity)
+		$Camera.rotation = v_camera_rotation(-event.relative.y/mouse_sensitivity)
 
 
 func move():
@@ -29,7 +45,7 @@ func move():
 
 
 func face_forward(x, z):
-	rotation.y = atan2(x,z) # - (PI/2)
+	$Armature.rotation.y = atan2(x,z)- (PI/2)
 
 
 func animate():
@@ -41,8 +57,15 @@ func animate():
 	movement_state = clamp(movement_state, 0, 1)
 	var animation = $Armature/AnimationTree
 	animation["parameters/Blend2/blend_amount"] = movement_state
+
+
+func h_camera_rotation(camera_rotation):
+	return rotation + Vector3(0,camera_rotation, 0)
 	
-	
-	
-	
-	
+func v_camera_rotation(camera_rotation):
+	var rot = $Camera.rotation + Vector3(camera_rotation, 0, 0)
+	rot.x = clamp(rot.x, PI/-8, PI/8)
+	return rot
+
+
+
